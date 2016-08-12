ecf961e04cf3b614a8d6a2c3d8986a37923d4671
Eric Burleson <eric.burleson@gmail.com>
2016-08-12 15:17:54 -0500

    This is from Chapter 2 of Pine's book

---
+++ LearntoProgram2a.rb
@@ -0,0 +1,29 @@
+#LearntoProgram.rb
+
+puts 1+2
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1 + 2 
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+#Hours in a year:
+hours = 24 * 365.25
+puts hours
+
+#minutes in a decade
+minutes = 60 * 24 * 365.25 * 10
+puts minutes
+
+#my age in seconds
+seconds = (60 * 60 * 24 * 365.25 * 34) + ((60 * 60 * 24) * (23 + 31 + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 12))
+p seconds
+
+#Author's age
+age = 1025000000 / (60 * 60 * 24 * 365.25) 
+p age
